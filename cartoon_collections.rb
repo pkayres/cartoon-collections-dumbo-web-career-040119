7dc3eb64e9608af236a838f54b680251c20a15f8
Pete A <peter.ayresmusic@gmail.com>
2019-03-12 20:37:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,14 +22,14 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    found = array.each { |item|
+    found = array.each do |item|
       if cheese_types.include?(item)
-     return item
-   }
+        return item
+   end
+ end
     if found != nil
-      return found 
-        else
-          return nil
+      return found
+    else
+      return nil
     end
-  end
   end
